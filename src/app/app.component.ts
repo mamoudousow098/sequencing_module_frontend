80e431ad292f9294aa5bbb5d650c3b5d5cf19834
mamoudousow098 <mamoudoumamadousow@gmail.com>
2023-01-04 10:04:02 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'sequencing_module_app';
+  title = 'Sequencing Module App';
 }
